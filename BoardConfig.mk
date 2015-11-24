203d58eeaf5a102eb05abc9b8644b07d8dd399da
martinusbe <martinusbe@gmail.com>
2015-11-24 15:16:21 +0100

    add some opts
    Change-Id: Ia940bd723c3da1ac57adf4d7f651e087be6f9f3f
    Signed-off-by: martinusbe <martinusbe@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,12 +21,17 @@ PLATFORM_PATH := device/oneplus/bacon
 
 # Validus additions
 BLOCK_BASED_OTA=false
+CLANG_O3 := true
+STRICT_ALIASING := false
+KRAIT_TUNINGS := true
+GRAPHITE_OPTS := true
+ENABLE_GCCONLY := true
 
 # Kernel Toolchain
 KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-5.2-uber/bin
 KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
 # Rom Toolchain
-TARGET_GCC_VERSION_EXP := 4.9-uber
+#TARGET_GCC_VERSION_EXP := 4.9-uber
 
 # Include path
 TARGET_SPECIFIC_HEADER_PATH := $(PLATFORM_PATH)/include
